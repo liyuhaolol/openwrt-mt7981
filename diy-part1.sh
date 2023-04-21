3587ff8e810d339361bceae98c2bd667329ec7fa
Flash <liyuhaoid@163.com>
2023-04-21 20:26:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git^b292dcafd5706c9f0545dc1848b34d3b7c8be49d' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;packages^6875e1b1f77f34cf538e5fecb70f65017ae6830c' >>feeds.conf.default
 echo 'src-git passwallluci https://github.com/xiaorouji/openwrt-passwall.git;luci^37f598eec05a463e3ab782e40ee722ba2cb6069e' >>feeds.conf.default
